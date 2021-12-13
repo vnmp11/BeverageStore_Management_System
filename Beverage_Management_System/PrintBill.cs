e63fc5c2519e728ae23de4de2482f1847fb10a7b
Quyên Phạm Ngọc <19522115@ms.uit.edu.vn>
2021-12-13 19:28:08 +0700

    StatisticAndPrint

---
+++ Beverage_Management_System/PrintBill.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Beverage_Management_System
+{
+    public partial class PrintBill : Form
+    {
+        public PrintBill()
+        {
+            InitializeComponent();
+        }
+
+        private void txb_quantity_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btt_ok_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2ImageButton1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
